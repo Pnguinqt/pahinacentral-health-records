3b808627cc15af6e26630b6807ce66780319f1ce
John Lawrence Dioquino <159016024+Penguinqt@users.noreply.github.com>
2026-06-16 11:37:48 +0800

    Reusable Input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Button from './components/button';
-
+import Input from './components/Input';
+import { useState } from "react";
+import  {FaEye, FaEyeSlash} from "react-icons/fa";
 function App() {
-  
+
+  const [email, setEmail] = useState("");  
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   return(
 
     <div className="mt-10 mx-10 gap-5 flex">
       <Button variant="primary" size="md">
       Save Patient
       </Button>
+
 
       <Button variant="secondary" size="md">
        Cancel
@@ -21,7 +27,35 @@ function App() {
       <Button variant="outline" size="md">
         View Details
       </Button>
+
+
+        <div className="flex gap-2">
+            <Input
+            label="Email"
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) =>setEmail(e.target.value)}
+            className="w-full"
+            />
+
+            <Input 
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full"
+            rightIcon={showPassword ? <FaEyeSlash /> : <FaEye />}
+            onRightIconClick={() => setShowPassword(!showPassword)}
+             />
+
+         </div>
     </div>
+
+  
   )
   
 }
